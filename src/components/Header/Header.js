3bc9c1e3b4389ddc919bf997825737f6132da4a6
Robert-Fish <elasys63+github@gmail.com>
2019-04-25 15:23:51 +1000

    - Added navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 export default function Header() {
-  return <div className="HeaderCont" />;
+  return (
+    <div className="HeaderCont">
+      <Navbar />
+      <p className="HeaderText">
+        Buy and Sell <br /> your property in <br /> one place!
+      </p>
+    </div>
+  );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className="NavBar">
+      <h1 className="NavHeaderText">HomeStead</h1>
+      <>
+        <p className="Nav-Item">Home</p>
+        <p className="Nav-Item">Features</p>
+        <p className="Nav-Item">Feedback</p>
+      </>
+    </div>
+  );
+}
